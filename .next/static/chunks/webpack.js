daf114cbf2fc6f1a0162cf753c040dc4091f8d54
yujis <yuji.sakata0110@gmail.com>
2022-10-20 11:25:10 +0900

    fixed url

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f65f1ddef938b96"; }
+/******/ 		__webpack_require__.h = function() { return "c4c6c0b98b08ed83"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/common/nav.jsx
+++ components/common/nav.jsx
@@ -10,7 +10,7 @@ export default function Navigation() {
                             <a className="nav-link" href="/">Home</a>
                         </li>
                         <li className="navi">
-                            <a className="nav-link" href="https://github.com/Yuji-Sakata0110/myportfolio">
+                            <a className="nav-link" href="https://github.com/Yuji-Sakata0110/portfolio_20221020">
                                 <img src="/images/github.png" className="navi-github" alt="" width="20px" height="20px" />
                                 Source
                             </a>
